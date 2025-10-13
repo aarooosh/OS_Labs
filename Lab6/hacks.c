c76bd6048affa2a1562eda9cbf1cc291c3900e38
A_Ghate <98141133+aarooosh@users.noreply.github.com>
2025-10-13 21:41:15 +0530

    Update hacks.c

--- Lab6/hacks.c
+++ Lab6/hacks.c
@@ -6,11 +6,12 @@
 #include<memory.h>
 
 struct hack_config{
-	             long cur_hack_config;
-		     u64 usr_handler_addr;
+			//struct to store the hack configuration and user handler function's address
+	        long cur_hack_config;
+		    u64 usr_handler_addr;
 };
 
-//Used to store the current hack configuration
+//Used to store the current hack configuration which is defaulted to -1,-1
 static struct hack_config hconfig = {-1, -1};
 
 
@@ -21,6 +22,8 @@ static struct hack_config hconfig = {-1, -1};
 
 long sys_config_hs(struct exec_context *ctx, long hack_mode, void *uhaddr)
 {
+	//this fucntion configures the value in the struct hconfig after validating
+	//a lot of the ifs are superflous , we could've just stored it after an OR
 	if(hack_mode==DIV_ZERO_OPER_CHANGE){
 		hconfig.cur_hack_config=hack_mode;	
 	}
@@ -49,7 +52,7 @@ long sys_config_hs(struct exec_context *ctx, long hack_mode, void *uhaddr)
 		//if(check valid code address)
 	}
 	else{
-		hconfig.cur_hack_config=-1129;
+		hconfig.cur_hack_config=-1129; //this is set to -1129 a randomly chosen invalid config number
 		return -EINVAL;
 	}
 	
@@ -66,41 +69,76 @@ long sys_config_hs(struct exec_context *ctx, long hack_mode, void *uhaddr)
 int do_div_by_zero(struct user_regs *regs)
 {	
 	if(hconfig.cur_hack_config==DIV_ZERO_OPER_CHANGE){
-		if(regs->rcx==0){//this is not needed
+		if(regs->rcx==0){
+			//the if is actually unnecessary since we assume correctness of the hardware error handler
+			//i.e. rcx had to have been zero for this to have been called in the first place
+			//Note : the assembly instruction does rax = rax/rcx (we got this from running the objdump)
+			//since the q says OPERCHANGE , we change the value of the registers so that when the hardware executes (remember !
+			//it's going to come back to the same div instruction since we've not changed the register of Instruction Pointer!)
+			//when it executes that again , it's a valid (although tampered) division
 			regs->rax=0;
 			regs->rcx=1;
 		}	
 	}	
 	else if(hconfig.cur_hack_config==DIV_ZERO_SKIP){
 		regs->entry_rip+=3;
+		//this is because the instruction consumed 3bytes (i guess this x86 64 has variable length instructions)
 //		printk("\nregs ka do_div_by_zero mai memory yeh hai: %x\n",regs);
 	}
 	else if(hconfig.cur_hack_config==DIV_ZERO_USH_EXIT){
+		//note : when we call a function , it looks for the passed arguments at standard locations (since they are being passed
+		//from external to the function right ?) which is rdi,rci,r8,r9 etc (i forget the exact ones)
+		//we're assured that the user handler will call exit itself so we don't have to worry about exiting the process
+		//but if we didn't have this assurance , i'm not too sure how we would've done it?
+		//because if it's a dummy handler which does nothing , it's possible to get stuck in an infinite loop
+		//perhaps we could have a check of if the same process at the same instruction is causing this error and kill that
+		//process from the OS handler?
 		regs->rdi=regs->entry_rip;
 		regs->entry_rip=hconfig.usr_handler_addr;
 	}	
 	else if(hconfig.cur_hack_config==DIV_ZERO_SKIP_FUNC){
+		//some of this poointer gymnastics is superflous
+		/**
+|			  |
+| 			  |
+|------------ | <----- rsp
+| locals 	  |
+| for func2   |
+| ... 		  |
+--------------- <------rbp
+| saved rbp   |
+---------------
+|RetAddr_func1|
+---------------
+|			  |
+| Stack Frame |
+| for func1   |
+| ...         |
+
+now the new base pointer value is set to the saved rbp which can be found by dereferencing the current rbp value
+the instruction pointer now points to RetAddr_func1 so that the rest of the function is skipped
+entry_rsp i.e. stack pointer at time of entry back into the current context should be set to rbp + 2 spaces , or rbp+16
+since it's x86 64 (8 bytes)
+it's also expected that the skipped function returns 1 and again , since it's returning to a space outside it's 
+original domain , it writes the value to a standard register , in this case it's rax
+hence we manually set rax=1
+
+			*/
 		u64 a=regs->rbp;// meditate on it
-	        //regs->rbp=*(u64*)regs->rbp;
+	    //regs->rbp=*(u64*)regs->rbp;
 		u64* p=(u64*)regs->rbp;
 		regs->rbp=*(u64*)p;
 		regs->entry_rip=*(p+1);
-		
 		//regs->rbp=*(u64*)a;
 		//regs->entry_rip=*(u64*)a+1;
 		regs->entry_rsp=a+16;
 		regs->rax = 1;	
-		//regs->entry_rsp=a+2;
-
-		
+		//regs->entry_rsp=a+2;	
 	}
-
-
-	
 	else{
 		printk("Error...exiting\n");
+		//question specifies this !!! pay attention
 		do_exit(0);
-	}
-	
+	}	
     return 0;   	
 } 
